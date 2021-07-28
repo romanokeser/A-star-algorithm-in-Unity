c9710280fa4756a65469e45d46a290574753bf1a
romanokeser <romanokeser@gmail.com>
2021-07-28 20:18:34 +0200

    First commit with basic elements, obstacles, Node and GridScript scripts

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour
+{
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+
+    }
+     
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+    }
+}
