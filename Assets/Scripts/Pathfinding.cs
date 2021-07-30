b95835966148496818fa49263eec8ffa443a84de
romanokeser <romanokeser@gmail.com>
2021-07-30 13:51:16 +0200

    algorithm still in progress, foreach neighbour part finished

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    GridScript grid;
+    void Awake()
+    {
+        grid = GetComponent<GridScript>();
+        { 
+
+        }
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost) 
+                {
+                    currentNode = openSet[i];
+                }
+            }
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+            if (currentNode == targetNode)
+            {
+                return;
+            }
+        }
+    }
+}
